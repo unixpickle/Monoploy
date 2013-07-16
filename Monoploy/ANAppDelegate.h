ebecbcf0ca3ce075f2ba6c9ba5294b7661935427
Alex Nichol <alexnichol@comcast.net>
2013-07-16 09:33:57 -0400

    Initial Commit

---
+++ Monoploy/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  Monoploy
+//
+//  Created by Alex Nichol on 7/16/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
